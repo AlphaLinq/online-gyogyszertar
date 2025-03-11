d48fbb4270bc49ab14c17b523d76236b3838803e
AlphaLinq <szomszed.daniel@gmail.com>
2025-03-11 23:00:02 +0100

    komponensek létrehozása

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NgIf } from '@angular/common';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent,MenuComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'online-gyogyszertar';
+
+  page: string = "home";
 }
